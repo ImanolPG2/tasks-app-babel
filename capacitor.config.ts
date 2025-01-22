309d88da0f590f59106fead5b182ca4dadc17842
ImanolPG2 <imanolguzman75@gmail.com>
2025-01-22 12:40:29 -0400

    Se esta subiendo el build de la aplicacion en android y agregando los camdos correspodiente en readmi para arrancar el proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tasks-crud',
+  webDir: 'www'
+};
+
+export default config;
